81728108845e99abb072c68232b50d6da44c83a0
Protegido <90720771+Protegido@users.noreply.github.com>
2026-01-14 18:20:00 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,4 @@
+const name = "Ojo Mu'adh"
+const age = 23
+const favLang = "Python"
+console.log("My name is " + name + ", I'm " + age + " years old and my favorite programming language is " + favLang + ".")
